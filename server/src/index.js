03b1d342e98d3746044c0e46ef97e6155647fb1e
Lakshay Pareek <lakpareek@gmail.com>
2025-03-17 15:18:56 +0530

    add start script for server

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.json({ message: "Server is working" });
+  res.send('Hello World')
 });
 
 app.use('/api/pdf', pdfRoutes);
