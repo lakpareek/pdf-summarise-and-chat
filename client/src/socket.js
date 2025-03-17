18b013407a0feb3f76fb85f990d21a767090c582
Lakshay Pareek <lakpareek@gmail.com>
2025-03-17 15:55:16 +0530

    add socket client dependency

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,7 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = "localhost:3001";
+const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
 
 export const socket = io(URL, {
-    autoConnect: true
-  });
+  autoConnect: true
+});
